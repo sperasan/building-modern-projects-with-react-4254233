01afc03f84a31f64bfe9ceda204a9d8bc25702b2
sean-golark <56940505+sperasan@users.noreply.github.com>
2026-06-03 11:14:51 +0800

    Styled-Components

--- todo-list-app/src/TodoListItem.jsx
+++ todo-list-app/src/TodoListItem.jsx
@@ -1,11 +1,19 @@
 import { useDispatch } from "react-redux";
 import { markTodoAsCompleted, deleteTodo } from "./thunks";
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  ${(props) => props.important && "background-color: yellow;"}
+  border-radius: 10px;
+  box-shadow: 0 2px 5px rgba(255, 255, 255, 0.75);
+  padding: 16px;
+`;
 
 const TodoListItem = ({ todo }) => {
   const dispatch = useDispatch();
 
   return (
-    <div>
+    <CardContainer important={todo.name.endsWith("!")}>
       <p>{todo.name}</p>
       {todo.isCompleted && <p>Complete!</p>}
       {todo.isCompleted ? (
@@ -17,7 +25,7 @@ const TodoListItem = ({ todo }) => {
           Mark as Completed
         </button>
       )}
-    </div>
+    </CardContainer>
   );
 };
 
